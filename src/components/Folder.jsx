50cf151e70d3e57baf9e631405246384cf73f2c0
mohsin19295 <mohsin19295@gmail.com>
2022-07-24 01:53:38 +0530

    style done

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,34 +1,55 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import styled from "styled-components"
+import { FcOpenedFolder } from "react-icons/fc";
+import { BsFileEarmarkRichtext } from "react-icons/bs";
+
+
+const Div = styled.div`
+  background-color: #313133;
+  color: #fff;
+  cursor: pointer;
+  max-width: 200px;
+`;
+
+const P = styled.p`
+    margin: 0;
+    padding: 0.3px;
+  &:hover {
+    background-color: #969696;
+  }
+`;
 
 const Folder = ({json}) => {
     const [expand, setExpand] = useState(false)
     
     if (json.isFolder) {
         return (
-          <div>
-            <span
-              onClick={() => {
-                setExpand(!expand);
-              }}
-            >
-              {json.name}
-            </span>
-
-              {json.items.map((exp) => (
-                <div
-                  key={exp.name}
-                  style={{
-                    display: expand ? "block" : "none",
-                    paddingLeft: "10px",
+          <Div>
+            <div style={{display: 'flex', gap: '5px'}}>
+              <FcOpenedFolder />
+                <P
+                  onClick={() => {
+                    setExpand(!expand);
                   }}
                 >
-                  <Folder json={exp} />
-                </div>
-              ))}
-          </div>
+                  {json.name}
+                </P>
+            </div>
+            {json.items.map((exp) => (
+              <Div
+                key={exp.name}
+                style={{
+                  display: expand ? "block" : "none",
+                  paddingLeft: "10px",
+                }}
+              >
+                <Folder json={exp} />
+              </Div>
+            ))}
+          </Div>
         );
     } else {
-        return <span>{json.name}</span>;
+        return <P>{json.name}</P>;
  }
 }
 
